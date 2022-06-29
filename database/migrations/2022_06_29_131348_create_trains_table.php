dce034e293f5861686d97469ffd62806dcf14a60
vittorioaquilino <vittorioaquilino17@gmail.com>
2022-06-29 15:28:30 +0200

    creato database e creata la migration con aggiunta di dati

---
+++ database/migrations/2022_06_29_131348_create_trains_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTrainsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('Azienda', 100);
+            $table->string('Stazione_di_partenza', 50);
+            $table->string('Stazione_di_arrivo', 50);
+            $table->date('Orario_di_partenza');
+            $table->date('Orario_di_arrivo');
+            $table->unsignedInteger('Codice_Treno');
+            $table->unsignedTinyInteger('Numero_Carrozze');
+            $table->boolean('In_orario')->default(true);
+            $table->boolean('Cancellato')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+}
